1fae37aba031af752756a44f0c51e2140b037fcc
David Magalhães <81379039+davmag91@users.noreply.github.com>
2023-03-08 15:21:06 +0000

    add floating buttons so we can change visual of adding new expenses

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 // ignore_for_file: prefer_const_constructors
+import 'dart:math';
 
-import 'package:expenses/components/transaction_list.dart';
-import 'package:expenses/components/transaction_user.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import './models/transaction.dart';
 import 'components/transaction_form.dart';
+import 'models/transaction.dart';
+import 'components/transaction_list.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -21,14 +22,51 @@ class ExpensesApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   MyHomePage();
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tênis de Corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+  ];
+
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+
+    setState(() => _transactions.add(newTransaction));
+  }
+
+  _openTransactionFormModal(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (ctx) {
+          return TransactionForm(onSubmit: null);
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Despesas Pessoais'), actions: <Widget>[
-        IconButton(onPressed: () {}, icon: Icon(Icons.add))
+        IconButton(
+            onPressed: () {
+              _openTransactionFormModal(context);
+            },
+            icon: Icon(Icons.add))
       ]),
       body: SingleChildScrollView(
         child: Column(
@@ -42,12 +80,14 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            TransactionUser()
+            TransactionList(_transactions),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          _openTransactionFormModal(context);
+        },
         child: Icon(Icons.add),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
