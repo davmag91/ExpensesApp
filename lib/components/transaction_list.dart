bb2cf6a0560446505923d7e3e783ea5d807abd87
David Magalhães <81379039+davmag91@users.noreply.github.com>
2023-03-08 17:18:07 +0000

    Add image and fixing some issues

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -15,51 +14,73 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: ListView.builder(
-          itemCount: transactions.length,
-          itemBuilder: (context, index) {
-            final tr = transactions[index];
-            return Card(
-                child: Row(
+      child: transactions.isEmpty
+          ? Column(
               children: [
-                Container(
-                  child: Container(
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                      color: Theme.of(context).primaryColor,
-                      width: 2,
-                    )),
-                    padding: EdgeInsets.all(10),
-                    margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                    child: Text(
-                      '${tr.value.toStringAsFixed(2)} €',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Theme.of(context).primaryColor,
-                      ),
-                    ),
-                  ),
+                SizedBox(
+                  height: 20,
                 ),
-                Column(
-                  children: <Widget>[
-                    Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(tr.title,
-                              style: Theme.of(context).textTheme.titleMedium),
-                          Text(
-                            DateFormat('d MMM y').format(tr.date),
-                            style: TextStyle(
-                              color: Colors.grey,
-                            ),
-                          )
-                        ])
-                  ],
+                Text(
+                  'Nothing to do here',
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: 200,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
+                  ),
                 )
               ],
-            ));
-          }),
+            )
+          : ListView.builder(
+              itemCount: transactions.length,
+              itemBuilder: (context, index) {
+                final tr = transactions[index];
+                return Card(
+                    child: Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                        color: Theme.of(context).primaryColor,
+                        width: 2,
+                      )),
+                      padding: const EdgeInsets.all(10),
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 15, vertical: 10),
+                      child: Text(
+                        '${tr.value.toStringAsFixed(2)} €',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(tr.title,
+                                  style:
+                                      Theme.of(context).textTheme.titleMedium),
+                              Text(
+                                DateFormat('d MMM y').format(tr.date),
+                                style: const TextStyle(
+                                  color: Colors.grey,
+                                ),
+                              )
+                            ])
+                      ],
+                    )
+                  ],
+                ));
+              }),
     );
   }
 }
