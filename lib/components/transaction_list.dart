3bcfc9588e4881e6cbe4bdac037ed0468ace8fe6
David Magalhães <81379039+davmag91@users.noreply.github.com>
2023-03-08 18:26:14 +0000

    Listtile

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -41,45 +41,25 @@ class TransactionList extends StatelessWidget {
               itemBuilder: (context, index) {
                 final tr = transactions[index];
                 return Card(
-                    child: Row(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                        color: Theme.of(context).primaryColor,
-                        width: 2,
-                      )),
-                      padding: const EdgeInsets.all(10),
-                      margin: const EdgeInsets.symmetric(
-                          horizontal: 15, vertical: 10),
-                      child: Text(
-                        '${tr.value.toStringAsFixed(2)} €',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Theme.of(context).primaryColor,
-                        ),
-                      ),
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 5,
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                          padding: EdgeInsets.all(6),
+                          child: FittedBox(child: Text('${tr.value} €'))),
                     ),
-                    Column(
-                      children: <Widget>[
-                        Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(tr.title,
-                                  style:
-                                      Theme.of(context).textTheme.titleMedium),
-                              Text(
-                                DateFormat('d MMM y').format(tr.date),
-                                style: const TextStyle(
-                                  color: Colors.grey,
-                                ),
-                              )
-                            ])
-                      ],
-                    )
-                  ],
-                ));
+                    title: Text(
+                      tr.title,
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                    subtitle: Text(DateFormat('d MMM y').format(tr.date)),
+                  ),
+                );
               }),
     );
   }
